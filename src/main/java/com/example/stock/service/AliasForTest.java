c61a433e44ce1ff340b65c4e534a6de3288a1216
nakgyeom <himina1227@gmail.com>
2023-01-05 09:14:47 +0900

    - 메타 어노에티션 제정의

---
+++ src/main/java/com/example/stock/service/AliasForTest.java
@@ -0,0 +1,25 @@
+package com.example.stock.service;
+
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author : nakgyeom
+ * @date : 2023-01-05 오전 8:45
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface AliasForTest {
+
+    //메타 어노테이션의 메서드를 재정의 -> 메타 어노테이션이란: 어노테이션을 선언할 때 사용하는 어노테이션
+    @AliasFor(annotation = RequestMapping.class, attribute = "method")
+    RequestMethod[] action() default {};
+
+    // 어노테이션 내의 암시적 별칭
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String[] value() default {};
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String[] route() default {};
+}
---
+++ src/main/java/com/example/stock/service/OptimisticLockService.java
@@ -0,0 +1,11 @@
+package com.example.stock.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : nakgyeom
+ * @date : 2023-01-05 오전 8:26
+ */
+@Service
+public class OptimisticLockService {
+}
